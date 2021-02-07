cfbe60f7c64375fc257cea76ae32545d7cef3cad
carterrc44 <30368872+carterrc44@users.noreply.github.com>
2021-02-07 08:40:49 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,15 @@ node default {
   file {'/root/README':
     ensure => file,
     content => 'This is a README',
-    owner => 'root,
+    owner => 'root',
   }
+}
+node 'master.puppet.vm' {
+  include role::master_server
+}
+node /^web/ {
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
 }
